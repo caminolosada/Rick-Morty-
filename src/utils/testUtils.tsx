9e12bcb4b75ede3876d3e0e0a160636b5001bfa7
Camino Losada <camino.losada.alonso@gmail.com>
2023-10-29 18:27:48 +0100

    Add testUtils with renderWithProviders & wrapWithRouter

---
+++ src/utils/testUtils.tsx
@@ -0,0 +1,40 @@
+import { PreloadedState } from "@reduxjs/toolkit";
+import { RootState, setupStore, store } from "../store";
+import { PropsWithChildren } from "react";
+import { render } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/theme";
+import GlobalStyle from "../styles/GlobalStyle/GlobalStyle";
+import { Provider } from "react-redux";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+
+export const renderWithProviders = (
+  ui: React.ReactElement,
+  preloadedState?: PreloadedState<RootState>
+) => {
+  const testStore = preloadedState ? setupStore(preloadedState) : store;
+
+  const Wrapper = ({ children }: PropsWithChildren): React.ReactElement => {
+    return (
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Provider store={testStore}>{children}</Provider>
+      </ThemeProvider>
+    );
+  };
+
+  render(ui, { wrapper: Wrapper });
+};
+
+export const wrapWithRouter = (ui: React.ReactElement) => {
+  const routes = [
+    {
+      path: "/",
+      element: ui,
+    },
+  ];
+
+  const router = createMemoryRouter(routes);
+
+  return <RouterProvider router={router} />;
+};
