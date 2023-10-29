21c59d54d50813626f7bd65c56e3675400770d47
Camino Losada <camino.losada.alonso@gmail.com>
2023-10-29 15:22:51 +0100

    Add ContainerStyled

---
+++ src/components/shared/ContainerStyled.ts
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+const ContainerStyled = styled.div`
+  margin: 0 auto;
+  padding: 29;
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.colors.background};
+`;
+
+export default ContainerStyled;
