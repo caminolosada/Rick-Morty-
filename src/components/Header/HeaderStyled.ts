b61c755a2a1e1f2a1c6a72d0fb3195ba31f6a85c
Camino Losada <camino.losada.alonso@gmail.com>
2023-10-29 16:13:12 +0100

    Add Header and its syles

---
+++ src/components/Header/HeaderStyled.ts
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: center;
+  margin-top: 17px;
+`;
+
+export default HeaderStyled;
