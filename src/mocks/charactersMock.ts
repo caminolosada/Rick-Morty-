32930988f0c490dbc109584cf54db1a24a6f3f6a
Camino Losada <camino.losada.alonso@gmail.com>
2023-10-30 18:51:51 +0100

    Add charactersSlice.test with a case for loadCharacters minireducer

---
+++ src/mocks/charactersMock.ts
@@ -0,0 +1,20 @@
+import { CharacterStructure } from "../types";
+
+export const charactersMocks: CharacterStructure[] = [
+  {
+    id: 1,
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    name: "Rick Sanchez",
+    species: "Human",
+    location: "Citadel of Ricks",
+    status: "Alive",
+  },
+  {
+    id: 2,
+    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    name: "Morty Smith",
+    species: "Human",
+    location: "Citadel of Ricks",
+    status: "Alive",
+  },
+];
