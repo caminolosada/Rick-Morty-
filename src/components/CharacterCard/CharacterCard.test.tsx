df473d6df6d8ddb4547914a8b388e1d9517b964e
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-03 17:39:50 +0100

    Fix tests affected by changes in charactersMock

--- src/components/CharacterCard/CharacterCard.test.tsx
+++ src/components/CharacterCard/CharacterCard.test.tsx
@@ -4,20 +4,22 @@ import { renderWithProviders } from "../../utils/testUtils";
 import CharacterCard from "./CharacterCard";
 
 beforeEach(() => {
-  renderWithProviders(<CharacterCard characterProps={charactersMock[0]} />);
+  renderWithProviders(
+    <CharacterCard characterProps={charactersMock.results[0]} />
+  );
 });
 
 describe("Given a CharacterCard component", () => {
   describe("When it receives the character 'Rick Sanchez'", () => {
     test("Then it should show an image with the alternative text 'Rick Sanchez'", () => {
-      const expectedAlternativeText = `${charactersMock[0].name}`;
+      const expectedAlternativeText = `${charactersMock.results[0].name}`;
 
       const image = screen.getByAltText(expectedAlternativeText);
 
       expect(image).toBeInTheDocument();
     });
     test("Then it should show the character's name", () => {
-      const expectedCharacterName = `${charactersMock[0].name}`;
+      const expectedCharacterName = `${charactersMock.results[0].name}`;
 
       const characterName = screen.getByRole("heading", {
         name: expectedCharacterName,
@@ -26,7 +28,7 @@ describe("Given a CharacterCard component", () => {
     });
 
     test("Then it should show the character's specie", () => {
-      const expectedCharacterSpecie = `${charactersMock[0].species}`;
+      const expectedCharacterSpecie = `${charactersMock.results[0].species}`;
 
       const characterSpecie = screen.getByText(expectedCharacterSpecie);
 
@@ -34,7 +36,7 @@ describe("Given a CharacterCard component", () => {
     });
 
     test("Then it should show the character's ubication", () => {
-      const expectedCharacterUbication = `${charactersMock[0].location}`;
+      const expectedCharacterUbication = `${charactersMock.results[0].location.name}`;
 
       const characterUbication = screen.getByText(expectedCharacterUbication);
 
