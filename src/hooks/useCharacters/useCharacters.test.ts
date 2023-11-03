8858128a82623f2a19ff7d8994847772da493611
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-03 16:28:55 +0100

    Add error case to useCharacters.test suite

--- src/hooks/useCharacters/useCharacters.test.ts
+++ src/hooks/useCharacters/useCharacters.test.ts
@@ -1,6 +1,8 @@
 import { renderHook } from "@testing-library/react";
 import { charactersMock } from "../../mocks/charactersMock";
 import useCharacters from "./useCharacters";
+import { server } from "../../mocks/node";
+import { errorHandlers } from "../../mocks/handlers";
 
 describe("Given an useCharacters function", () => {
   describe("When it calls the getCharacters function", () => {
@@ -16,6 +18,19 @@ describe("Given an useCharacters function", () => {
       const response = await getCharacters();
 
       expect(response).toStrictEqual(expectedResponse);
+    });
+  });
+  describe("When the getCharacters function is called and an error occurs", () => {
+    test("Then it should throw an error", () => {
+      server.resetHandlers(...errorHandlers);
+
+      const {
+        result: {
+          current: { getCharacters },
+        },
+      } = renderHook(() => useCharacters());
+
+      expect(getCharacters()).rejects.toThrowError();
     });
   });
 });
