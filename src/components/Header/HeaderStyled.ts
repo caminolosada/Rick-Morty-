469dc21e80c1a403114945cdea12f8cf66dba8e0
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-04 12:03:20 +0100

    Add responsivity to Header component

--- src/components/Header/HeaderStyled.ts
+++ src/components/Header/HeaderStyled.ts
@@ -3,7 +3,19 @@ import { styled } from "styled-components";
 const HeaderStyled = styled.header`
   display: flex;
   justify-content: center;
-  margin-top: 17px;
+  padding: 50px 0px;
+
+  .logo {
+    @media (min-width: 600px) {
+      width: 393px;
+      height: 95.76px;
+    }
+
+    @media (min-width: 950px) {
+      width: 524px;
+      height: 127.68px;
+    }
+  }
 `;
 
 export default HeaderStyled;
