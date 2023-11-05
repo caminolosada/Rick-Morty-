4fadc9c71e2dcca8cbc5feb73681a3e28095db91
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-05 14:09:53 +0100

    Add use cases to test handleOnClick functionality in CharacterListPage

--- src/pages/CharacterListPage.test.tsx
+++ src/pages/CharacterListPage.test.tsx
@@ -1,6 +1,8 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { renderWithProviders, wrapWithRouter } from "../utils/testUtils";
 import CharacterListPage from "./CharacterListPage";
+import { store } from "../store";
 
 const renderCharacterListPage = () => {
   renderWithProviders(wrapWithRouter(<CharacterListPage />));
@@ -23,6 +25,36 @@ describe("Given a CharacterListPage", () => {
       const decoration = screen.getByAltText(expectedAlternativeText);
 
       expect(decoration).toBeInTheDocument();
+    });
+
+    test("Then it should show a button with the text 'Load more'", () => {
+      renderCharacterListPage();
+      const expectedText = "Load more";
+
+      const loadMoreButton = screen.getByRole("button", { name: expectedText });
+
+      expect(loadMoreButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When an user clicks on 'Load more' button", () => {
+    test("Then it should show the new chartacters' collecion", async () => {
+      renderCharacterListPage();
+      const expectedText = "Load more";
+      const expectedCharacterCollectionLength = 4;
+
+      const loadMoreButton = screen.getByRole("button", {
+        name: expectedText,
+      });
+
+      await userEvent.click(loadMoreButton);
+
+      const newCharactersCollectionLength =
+        store.getState().characters.results.length;
+
+      expect(newCharactersCollectionLength).toBe(
+        expectedCharacterCollectionLength
+      );
     });
   });
 });
