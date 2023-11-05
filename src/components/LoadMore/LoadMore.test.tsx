6d6c9c3785dee5d54331558fa6408cb7afde1ad6
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-05 13:34:21 +0100

    Add use case to test the logical funcionality of LoadMore component

--- src/components/LoadMore/LoadMore.test.tsx
+++ src/components/LoadMore/LoadMore.test.tsx
@@ -1,4 +1,6 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
 import { renderWithProviders } from "../../utils/testUtils";
 import LoadMore from "./LoadMore";
 
@@ -6,12 +8,27 @@ describe("Given a Button component", () => {
   describe("When it is rendered and receives the text 'Load more'", () => {
     test("Then it should show the received text inside", () => {
       const expectedText = "Load more";
+      const onClick = vi.fn();
 
-      renderWithProviders(<LoadMore />);
+      renderWithProviders(<LoadMore onClick={onClick} />);
 
       const loadMoreButton = screen.getByRole("button", { name: expectedText });
 
       expect(loadMoreButton).toBeInTheDocument();
+    });
+  });
+  describe("When it receives a function and an user clicks on it", () => {
+    test("Then it shoul call that function", async () => {
+      const expectedText = "Load more";
+      const onClick = vi.fn();
+
+      renderWithProviders(<LoadMore onClick={onClick} />);
+
+      const loadMoreButton = screen.getByRole("button", { name: expectedText });
+
+      await userEvent.click(loadMoreButton);
+
+      expect(onClick).toHaveBeenCalled();
     });
   });
 });
